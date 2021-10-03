6aecc985393e735cb3a35924d66d84d46f019c8d
Myznikova Nastya <myznikova.nastya@gmail.com>
2021-10-03 16:39:50 +0300

    changing the code language

--- 07-dichromatic-algorithm/07-dichromatic-algorithm/Source.cpp
+++ 07-dichromatic-algorithm/07-dichromatic-algorithm/Source.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
-#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-
 	int a=1, b=100;
     int c;
     int i=0;
     int p;
     
-    cout << "Задумайте число N от 1 до 100, а я угадаю! \n";
+    cout << "Think of a number N from 1 to 100, and I'll try to guess! \n";
     
     do
     {
         c=(a+b)/2;
-        cout << "N больше " << c << " (1 да, 2 нет, 3 равно)? \n";
+        cout << "N is bigger than " << c << " (1 yes, 2 no, 3 equels)? \n";
         cin >> p;
     
         if (p==1)
@@ -34,6 +31,6 @@ int main()
     }
     while (p!=3);
     
-	cout << "Вы задумали " << c << "! А я молодец! Количество попыток: " << i << " \n";
+	cout << "You thought of a number " << c << "I did good! Number of attempts:" << i << " \n";
 	return 0;
 }
